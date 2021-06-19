3ed480bcc78e3f9a4861f97f25c70e0273e0daef
Abimaelr <abimaelr.albu@gmail.com>
2021-06-19 16:40:34 -0300

    Testing api

--- api.js
+++ api.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const {results: data} = require('./data')
 
 const app = express();
 
 const PORT = process.env.PORT || 8877;
+function calculateIndex(x,y) {
+    const coordinate = [x,y];
+    const distances = data.map(({coordinates}) => {
+        const x = Math.pow((coordinates[0] - coordinate[0]),2);
+        const y = Math.pow((coordinates[1] - coordinate[1]),2);
+        return Math.sqrt((x+y));
+    })
+    const valueMin = Math.min(...distances);
+    const index = distances.findIndex((number) => number === valueMin)
+    return index;
+}
 
-app.get("/", (req,res) => {
-    res.json({msg: "Olá, Carlos Alberto!, Estamos trabalhando no desenvolvimento de nossa própria API para as aplicações da TRIX SOLAR"})
+
+
+app.get(`/coordinates/${lat},${lon}`, (req,res) => {
+    const response = data[calculateIndex(lat,lon)];
+    res.json(response)
 })
 
 app.listen(PORT, () => {
-    console.log('teste...');
+
+
 })
